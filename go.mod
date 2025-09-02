8c40eb8efa51f1ec69f53ac51c4032d866ca2d5e
Adis Veletanlic <adve@adisve.net>
2025-09-02 19:28:23 +0200

    Add proper bson mappings for shared models requiring it

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.23.0
 require (
 	github.com/PuerkitoBio/goquery v1.10.3
 	github.com/gin-gonic/gin v1.10.1
+	github.com/swaggo/files v1.0.1
+	github.com/swaggo/swag v1.8.12
 )
 
 require (
@@ -17,8 +19,6 @@ require (
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/swaggo/files v1.0.1 // indirect
-	github.com/swaggo/swag v1.8.12 // indirect
 	golang.org/x/tools v0.35.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- pkg/models/schedule.go
+++ pkg/models/schedule.go
@@ -5,29 +5,29 @@ import (
 )
 
 type Event struct {
-	ID           string      `json:"id"`
-	ScheduleId   string      `json:"schedule_id"`
-	Title        string      `json:"title"`
-	CourseId     string      `json:"course_id"`
-	CourseName   string      `json:"course_name"`
-	Teachers     []*Teacher  `json:"teachers"`
-	From         time.Time   `json:"from"`
-	To           time.Time   `json:"to"`
-	Locations    []*Location `json:"locations"`
-	LastModified time.Time   `json:"last_modified"`
-	IsSpecial    bool        `json:"is_special"`
+	ID           string      `json:"id" bson:"id"`
+	ScheduleId   string      `json:"schedule_id" bson:"schedule_id"`
+	Title        string      `json:"title" bson:"title"`
+	CourseId     string      `json:"course_id" bson:"course_id"`
+	CourseName   string      `json:"course_name" bson:"course_name"`
+	Teachers     []*Teacher  `json:"teachers" bson:"teachers"`
+	From         time.Time   `json:"from" bson:"from"`
+	To           time.Time   `json:"to" bson:"to"`
+	Locations    []*Location `json:"locations" bson:"locations"`
+	LastModified time.Time   `json:"last_modified" bson:"last_modified"`
+	IsSpecial    bool        `json:"is_special" bson:"is_special"`
 }
 
 type Teacher struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	ID        string `json:"id" bson:"id"`
+	FirstName string `json:"first_name" bson:"firstname"`
+	LastName  string `json:"last_name" bson:"last_name"`
 }
 
 type Location struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	Building string `json:"building"`
-	Floor    string `json:"floor"`
-	MaxSeats string `json:"max_seats"`
+	ID       string `json:"id" bson:"id"`
+	Name     string `json:"name" bson:"name"`
+	Building string `json:"building" bson:"building"`
+	Floor    string `json:"floor" bson:"floor"`
+	MaxSeats string `json:"max_seats" bson:"max_seats"`
 }
