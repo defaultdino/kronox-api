2e84504e07f713b2614a1f9995a051e830b3ecf8
Adis Veletanlic <adve@adisve.net>
2025-09-07 01:38:43 +0200

    Expand user struct with events/bookings

--- pkg/models/user/auth.go
+++ pkg/models/user/auth.go
@@ -1,17 +1,16 @@
 package user
 
+import booking "github.com/tumble-for-kronox/kronox-api/pkg/models/resource"
+
 type LoginRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
+// stored in mongo
 type User struct {
-	Name      string `json:"name"`
-	Username  string `json:"username"`
-	SessionID string `json:"session_id"`
-}
-
-type UserInfo struct {
-	Name     string `json:"name"`
-	Username string `json:"username"`
+	Name     string                `json:"name" bson:"name"`
+	Username string                `json:"username" bson:"username"`
+	Events   []*AvailableUserEvent `json:"events" bson:"events"`
+	Bookings []*booking.Booking    `json:"bookings" bson:"bookings"`
 }
